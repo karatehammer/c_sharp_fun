e7fbb2a0a3a6f1e843ea146acab1494349fc92bf
Nick Hammer <nhammer@gmail.com>
2018-02-12 12:23:56 -0600

    first commit from visual studio

---
+++ visual_c_sharp_practice/AutomationCore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace visual_c_sharp_practice
+{
+    [TestFixture]
+    public class AutomationCore
+    {
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void startTest()
+        {
+            
+            driver = new ChromeDriver();
+            driver.Url = "http://blog.testproject.io/";
+        }
+
+        [TearDown]
+        public void endtest()
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void blogTest()
+        {
+            NUnit.Framework.Assert.IsTrue(driver.Url.Contains("asdfasfd"));
+        }
+    }
+}
---
+++ visual_c_sharp_practice/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("visual_c_sharp_practice")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("visual_c_sharp_practice")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("20504381-534d-4819-943f-34d238417c46")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
